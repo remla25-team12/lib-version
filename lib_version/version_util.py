3da9c9b154bf9a4c4cc4275ca1af7f40ebfecce9
Ayushkuruvilla <ayush.kuruvilla@gmail.com>
2025-05-02 12:44:43 +0200

    Change in library name for pypi

--- lib_version/version_util.py
+++ lib_version/version_util.py
@@ -4,6 +4,6 @@ class VersionUtil:
     @staticmethod
     def get_version():
         try:
-            return importlib.metadata.version("lib-version")
+            return importlib.metadata.version("lib-version-remla25-team12")
         except importlib.metadata.PackageNotFoundError:
             return "dev"
